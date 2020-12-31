4a6b7948d41d624cccd21b0c84cd147be72511d2
Abdul Kudus <abdulkudus2922@gmail.com>
2020-12-31 19:23:04 +0000

    added default options to config

--- examples/mqtt.php
+++ examples/mqtt.php
@@ -1,6 +1,7 @@
 <?php
 
 use Morbo\React\Mqtt\Client;
+use Morbo\React\Mqtt\ConnectionOptions;
 use Morbo\React\Mqtt\Protocols\Version4;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -10,6 +11,7 @@ $loop = React\EventLoop\Factory::create();
 $config = [
     'host' => 'localhost',
     'port' => 1883,
+    'options' => new ConnectionOptions()
 ];
 
 $mqtt = new Client($loop, new Version4());
